716c0df20422a5e3e1f4fbcf7276d49f9f0955b1
unknown <velasquezjeisson@gmail.com>
2026-09-07 16:28:27 -0500

    ChurchStream-Control init.bat

--- init.bat
+++ init.bat
@@ -9,4 +9,4 @@ tasklist /FI "IMAGENAME eq OpenLP.exe" 2>NUL | find /I "OpenLP.exe" >NUL
 if errorlevel 1 if defined OPENLP_EXE start "" /min "%OPENLP_EXE%"
 if errorlevel 1 timeout /t 5 /nobreak >nul
 start "" http://127.0.0.1:8765/
-where py >nul 2>nul && py servidor.py || python server.py
+where py >nul 2>nul && py server.py || python server.py
